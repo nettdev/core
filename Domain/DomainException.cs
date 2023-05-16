06b9e7cac76284f326f5a13841b10b3ec7320616
Jeziel Moura <jezielmoura@outlook.com>
2023-05-16 11:12:30 -0300

    Add more methods

--- Domain/DomainException.cs
+++ Domain/DomainException.cs
@@ -14,37 +14,49 @@ public class DomainException : Exception
     public Error MapToError() =>
         new (Code, Message);
 
-    public static void ThrowIf(bool condition, string message, string code = "")
+    public static void ThrowIf(bool condition, string code, string message)
     {
         if (condition)
             throw new DomainException(code, message);
     }
 
-    public static void ThrowIfNull(object value, string message, string code = "")
+    public static void ThrowIfNull(object value, string code, string message)
     {
         if (value is null)
             throw new DomainException(code, message);
     }
 
-    public static void ThrowIfNullOrEmpty(string value, string message, string code = "")
+    public static void ThrowIfEmpty(Guid value, string code, string message)
+    {
+        if (Guid.Empty.Equals(value))
+            throw new DomainException(code, message);
+    }
+
+    public static void ThrowIfNullOrEmpty(string value, string code, string message)
     {
         if (string.IsNullOrEmpty(value))
             throw new DomainException(code, message);
     }
 
-    public static void ThrowIfNullOrEmpty<T>(IEnumerable<T> values, string message, string code = "")
+    public static void ThrowIfNullOrEmpty<T>(IEnumerable<T> values, string code, string message)
     {
         if (values is null || !values.Any())
             throw new DomainException(code, message);
     }
 
-    public static void ThrowIfEmpty<T>(IEnumerable<T> values, string message, string code = "")
+    public static void ThrowIfEmpty<T>(IEnumerable<T> values, string code, string message)
     {
         if (!values.Any())
             throw new DomainException(code, message);
     }
 
-    public static void ThrowIfNegative(double value, string message, string code = "")
+    public static void ThrowIfNegative(double value, string code, string message)
+    {
+        if (value < 0)
+            throw new DomainException(code, message);
+    }
+
+    public static void ThrowIfNegative(decimal value, string code, string message)
     {
         if (value < 0)
             throw new DomainException(code, message);
