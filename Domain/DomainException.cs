9cd95613e8cb5c5734ccffe65ac9f248bdfcb32e
Jeziel Moura <jezielmoura@outlook.com>
2023-05-16 11:05:12 -0300

    Add more exception static methods

--- Domain/DomainException.cs
+++ Domain/DomainException.cs
@@ -19,4 +19,34 @@ public class DomainException : Exception
         if (condition)
             throw new DomainException(code, message);
     }
+
+    public static void ThrowIfNull(object value, string message, string code = "")
+    {
+        if (value is null)
+            throw new DomainException(code, message);
+    }
+
+    public static void ThrowIfNullOrEmpty(string value, string message, string code = "")
+    {
+        if (string.IsNullOrEmpty(value))
+            throw new DomainException(code, message);
+    }
+
+    public static void ThrowIfNullOrEmpty<T>(IEnumerable<T> values, string message, string code = "")
+    {
+        if (values is null || !values.Any())
+            throw new DomainException(code, message);
+    }
+
+    public static void ThrowIfEmpty<T>(IEnumerable<T> values, string message, string code = "")
+    {
+        if (!values.Any())
+            throw new DomainException(code, message);
+    }
+
+    public static void ThrowIfNegative(double value, string message, string code = "")
+    {
+        if (value < 0)
+            throw new DomainException(code, message);
+    }
 }
