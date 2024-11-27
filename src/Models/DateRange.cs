806852435a95885e998755149af72839fcfe0188
Jeziel Moura <jezielmoura@outlook.com>
2024-11-27 18:47:29 -0300

    Fix date range type to support ef core

--- src/Models/DateRange.cs
+++ src/Models/DateRange.cs
@@ -1,11 +1,11 @@
 namespace Nett.Core;
 
-public readonly record struct DateRange
+public sealed record DateRange
 {
-    public DateOnly From { get; }
-    public DateOnly To { get; }
-    public readonly int Days => To.DayNumber - From.DayNumber;
-    public readonly IEnumerable<DateOnly> Dates => Enumerable.Range(0, Days).Select(From.AddDays);
+    public DateOnly From { get; private set; }
+    public DateOnly To { get; private set; }
+    public int Days => To.DayNumber - From.DayNumber;
+    public IEnumerable<DateOnly> Dates => Enumerable.Range(0, Days).Select(From.AddDays);
 
     public DateRange(DateOnly from, DateOnly to) => (From, To) = (from, to);
 }
