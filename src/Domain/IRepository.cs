8e46f5cb520975f4b018999048b998e1c72d51d9
Jeziel Moura <jezielmoura@outlook.com>
2025-11-22 22:24:54 -0300

    Add paged `Query` method to `IRepository<T>` interface

--- src/Domain/IRepository.cs
+++ src/Domain/IRepository.cs
@@ -1,3 +1,8 @@
+using Nett.Core.Models;
+
 namespace Nett.Core.Domain;
 
-public interface IRepository<T> where T : AggregateRoot;
+public interface IRepository<T> where T : AggregateRoot
+{
+    Task<PagedResponse<R>> Query<R>(PagedRequest<T, R> request, CancellationToken cancellation);
+}
